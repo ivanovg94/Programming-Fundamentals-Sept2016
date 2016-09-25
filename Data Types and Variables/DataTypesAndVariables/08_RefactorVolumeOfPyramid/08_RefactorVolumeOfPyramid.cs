3d0c05a1dde8476d548b9654a35fc48be7a53e60
ivanovg94 <georgipz94@abv.bg>
2016-09-25 03:02:01 +0300

    Added 08. Refactor Volume of Pyramid solution.

---
+++ Data Types and Variables/DataTypesAndVariables/08_RefactorVolumeOfPyramid/08_RefactorVolumeOfPyramid.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_RefactorVolumeOfPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Length: ");
+            double lenght = double.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            double width = double.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            double hight = double.Parse(Console.ReadLine());
+            double volume = (lenght * width * hight) / 3;
+            Console.WriteLine("Pyramid Volume: {0:F2}", volume);
+
+        }
+    }
+}
