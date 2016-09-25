956453dd45f4d375f3d3e444ebec8ca544f3788e
ivanovg94 <georgipz94@abv.bg>
2016-09-25 03:16:53 +0300

    Added 09 RefactorSpecialNumbers solution.

---
+++ Data Types and Variables/DataTypesAndVariables/09_RefactorSpecialNumbers/RefactorSpecialNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_RefactorSpecialNumbers
+{
+    class RefactorSpecialNumbers
+    {
+        static void Main(string[] args)
+        {
+            int range = int.Parse(Console.ReadLine());
+            int number = 0;
+            bool isSpecial = false;
+            for (int current = 1; current <= range; current++)
+            {
+                int result = 0;
+                number = current;
+                while (current > 0)
+                {
+                    result += current % 10;
+                    current = current / 10;
+                }
+                isSpecial = (result == 5) || (result == 7) || (result == 11);
+                Console.WriteLine($"{number} -> {isSpecial}");
+                result = 0;
+                current = number;
+            }
+
+        }
+    }
+}
